5467f57045238a3fd45b599f00e32421e558f4af
EmanuelCG <mr.emanuelcg@gmail.com>
2024-06-02 14:12:27 -0500

    The password change module is completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
 
     path('resetpassword/', views.resetpassword, name="resetpassword"),
     path('my_orders/', views.my_orders, name='my_orders'),
-    path('edit_profile/', views.edit_profile, name='edit_profile')
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('order_detail/<int:id>', views.order_detail, name='order_detail')
 ]
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
-from .forms import RegistrationForm, Account
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import RegistrationForm, UserForm, UserProfileForm
+from .models import Account, UserProfile
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
@@ -35,6 +36,13 @@ def register(request):
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #Create user profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
 
             # USER ACTIVATION
             current_site = get_current_site(request)
@@ -157,9 +165,11 @@ def activate(request, uidb64, token):
 def dashboard(request):
     orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
     orders_count = orders.count()
-
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    img_profile = userprofile.profile_picture.url
     context = {
         'orders_count': orders_count,
+        'img_profile': img_profile
     }
     return render(request, 'accounts/dashboard.html', context)
 
@@ -236,5 +246,50 @@ def my_orders(request):
     }
     return render(request, 'accounts/my_orders.html', context) 
 
+
+@login_required(login_url='login')
 def edit_profile(request):
-    return render(request, 'accounts/edit_profile.html')
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been updated')
+            return redirect('edit_profile')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile
+    }
+
+    return render(request, 'accounts/edit_profile.html', context)
+
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                messages.success(request, 'Password update successfully')
+                return redirect('change_password')
+            else:
+                messages.error(request, 'Please enter valid password')
+                return redirect('change_password')
+        else:
+            messages.error(request, 'Password does not match !')
+            return redirect('change_password')
+    return render(request, 'accounts/change_password.html')
